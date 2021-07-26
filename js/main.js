e73eeb389d92260af20f6cd26420b0f243a04a6c
YaraPolk <44962652+YaraPolk@users.noreply.github.com>
2021-07-26 12:54:06 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
-$(window).ready(function() {
-    $('div.post-name li:first-child').addClass('active-color');
-    $('div.post-content div:first-child ul').addClass('active');
-    $('div.post-content div:first-child span').addClass('active-color');
-    $('div.post-name li').on('click', function(){
-        $('div.post-content div ul').removeClass('active');
-        $('div.post-name li').removeClass('active-color');
-        $(this).addClass('active-color');
-        postName = $(this).text();
-        $('div.post-content div').each(function(){
-            if ($(this).text().includes(postName)) {
-                $(this).find('ul').addClass('active');
+jQuery(window).ready(function() {
+    jQuery('div.post-name li:first-child').addClass('active-color');
+    jQuery('div.post-content div:first-child ul').addClass('active');
+    jQuery('div.post-content div:first-child span').addClass('active-color');
+    jQuery('div.post-name li').on('click', function(){
+        jQuery('div.post-content div ul').removeClass('active');
+        jQuery('div.post-name li').removeClass('active-color');
+        jQuery(this).addClass('active-color');
+        postName = jQuery(this).text();
+        jQuery('div.post-content div').each(function(){
+            if (jQuery(this).text().includes(postName)) {
+                jQuery(this).find('ul').addClass('active');
             };
         });
     });
-    if ($(window).width() <= '650'){
-    $('div.post-content div span').on('click', function(){
-        $('div.post-content div ul').removeClass('active');
-        $('div.post-content span').removeClass('active-color');
-        $(this).addClass('active-color');
-        $(this).parents('div.post-content div').find('ul').addClass('active');
-        $(this + '::after').attr('content', '^');
+    if (jQuery(window).width() <= '650'){
+    jQuery('div.post-content div span').on('click', function(){
+       jQuery('div.post-content div ul').removeClass('active');
+        jQuery('div.post-content span').removeClass('active-color');
+        jQuery(this).addClass('active-color');
+        jQuery(this).parents('div.post-content div').find('ul').addClass('active');
+        jQuery(this + '::after').attr('content', '^');
     });
     };
 });
